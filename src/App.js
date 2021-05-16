b73fa740afc05a5c85ebeb8fbeef5ecd1f062f85
Neelesh Thakur <nt00605@gmail.com>
2021-05-16 17:30:04 +0530

    removed semantic ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import NavBar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      Blood  bank management
+      <NavBar />
     </div>
   );
 }
