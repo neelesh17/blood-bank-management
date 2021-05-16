181b312d8e675ab779f0b18d9dd8d8252787dd93
Neelesh Thakur <nt00605@gmail.com>
2021-05-16 17:41:37 +0530

    starrted navbbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './navbar.css';
+import logo from '../../logo.svg'
 
 const NavBar = () => {
-    return <div>
-        navbar
-    </div>
+    return (
+        <div className="navbarConatiner">
+            <div className="logoContainer"><img src={logo} alt="logo" /></div>
+            <div className="rightMenu">
+                <div className="loginButton">Login</div>
+                <div className="registerButton">Register</div>
+            </div>
+        </div>
+    )
 }
 
 export default NavBar;
